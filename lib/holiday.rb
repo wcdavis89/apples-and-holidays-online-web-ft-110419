d5498d543d08a2686b1778b8106b5afa1da04387
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 00:45:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.values.flatten
 end
 
 def all_holidays_with_bbq(holiday_hash)
