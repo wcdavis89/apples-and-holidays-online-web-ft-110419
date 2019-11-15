5bf46dc9815464174c0263251333b42dd91b82b6
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 01:05:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,11 +56,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, data|
+holiday_hash.each do |season, holiday|
    puts "#{season.capitalize}:"
-  data.each do |holiday,value|
-   puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
- end
+ holiday.each do |holiday, item|
+       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+     end
 end
 end
 
