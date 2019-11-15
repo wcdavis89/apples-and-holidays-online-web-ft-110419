849452c8a16c58c5cf375cbf33df9e216a8bd956
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 00:11:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 holiday_hash.each do |season,holiday|
   if season == :winter
     holiday.each do |supply|
-      holiday + supply
+      holiday<< supply
     end
   end
 end
