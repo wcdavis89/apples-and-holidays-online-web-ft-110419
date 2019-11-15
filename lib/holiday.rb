c59ab7563560ca36f943e396a4f75c392be8ae2d
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 00:16:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,9 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 holiday_hash[:winter].each do |holiday,supply|
       holiday.push(supply)
-    end
   end
-end
 end
 
 
